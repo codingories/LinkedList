906b68c5538bd8c47567ca0669cc149f63b87857
朱骏杰 <ories.zhu@mochain.vip>
2020-10-11 17:39:19 +0800

    first

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,10 @@
 import { defaultEquals } from "./util/defaultEquals"
 import {Node} from './models/linked-list-models'
 
+export default class LinkedList {
+  constructor(equalsFn = defaultEquals){
+    this.count = 0;
+    this.head = undefined;
+    this.equalsFn = equalsFn;
+  }
+}
